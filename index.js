3b65f0a8c4359a791721ad74c13f28576a267a18
Vinn Nduri <ndurivo@gmail.com>
2022-06-08 13:57:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+let multiply; let num1 = 31; let num2 = 2;
+    multiply = num1 * num2;
+
+let random;
+
+random = Number.isInteger(random) +1;
+
+
+let max = Math.max(2,4,6,8,10,20);
+
+
+
+let mod; let num3 = 14; let num4 = 5;
+    
+    mod = num3 % num4;
+
+
+
+
+
